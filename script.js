c6f00a71f4339206966dd1b127542232d067f21f
Aru2301 <159798633+Aru2301@users.noreply.github.com>
2024-09-22 20:50:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// script.js
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        const projectList = document.getElementById('project-list');
-        data.projects.forEach(project => {
-            const listItem = document.createElement('li');
-            listItem.textContent = project.name;
-            projectList.appendChild(listItem);
-        });
-    });
+
